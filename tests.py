c1972475015f6c6b3c80215e464e428cf95da371
Zach Seifts <zach.seifts@gmail.com>
2017-10-19 10:07:38 -0400

    Adding in tests for the startup class

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from jeep import Location
+from jeep import StartUp
 
 class TestLocationClass(unittest.TestCase):
     ''' A test class for testing the Location.
@@ -12,6 +13,12 @@ class TestLocationClass(unittest.TestCase):
         location = Location(longitude=123.1234, latitude=123.1234)
         self.assertEquals(location.longitude, 123.1234)
         self.assertEquals(location.latitude, 123.1234)
+        
+    def test_startup(self):
+        ''' Test the Startup class.
+        '''
+        startup = StartUp()
+        self.assertTrue(startup.running)
 
 
 if __name__ == '__main__':
