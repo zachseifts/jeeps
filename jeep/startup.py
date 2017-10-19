e062791377c5ea7b0101f0267ff473efbfbc8430
Zach Seifts <zach.seifts@gmail.com>
2017-10-19 10:05:29 -0400

    Create startup.py

---
+++ jeep/startup.py
@@ -0,0 +1,9 @@
+''' An object that runs on startup.
+'''
+
+class StartUp(object):
+    ''' The StartUp object is initiated once the jeep boots.
+    '''
+    
+    def __init__(self):
+        self.running = true
