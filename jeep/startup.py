387be0ae456504ed10d71676ff5df9043ef46168
Zach Seifts <zach.seifts@gmail.com>
2017-10-19 10:41:43 -0400

    Adding in call to syslog

--- jeep/startup.py
+++ jeep/startup.py
@@ -1,9 +1,12 @@
 ''' An object that runs on startup.
 '''
 
+from syslog import syslog
+
 class StartUp(object):
     ''' The StartUp object is initiated once the jeep boots.
     '''
     
     def __init__(self):
+        syslog('The jeep has started')
         self.running = True
