263dd790fecd335c6370588b5101d6bd3f756be9
Zach Seifts <zach.seifts@gmail.com>
2017-10-19 10:02:04 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,9 +9,9 @@ class TestLocationClass(unittest.TestCase):
     def test_init(self):
         ''' Test the init process for setting up a new Location.
         '''
-        location = Location()
-        self.assertIsNone(location.longitude)
-        self.assertIsNone(location.latitude)
+        location = Location(longitude=123.1234, latitude=123.1234)
+        self.assertEquals(location.longitude, 123.1234)
+        self.assertEquals(location.latitude, 123.1234)
 
 
 if __name__ == '__main__':
